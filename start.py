0e424f3f20bff6edd9ecd64ccd8c0bc34d8b8e37
wessware <stevenshisia@yahoo.com>
2022-05-06 12:17:42 +0300

    video read display

--- start.py
+++ start.py
@@ -1,9 +1,20 @@
+from tkinter import Frame
 import cv2 as cv
 
 
-img = cv.imread('./images/img_1.jpg')
+#img = cv.imread('./images/img_1.jpg')
 
 
-cv.imshow('Cat', img)
+#cv.imshow('Image_One', img)
+capture = cv.VideoCapture('./videos/vid_1.mp4')
 
-cv.waitKey(0)
+while True:
+    isTrue, frame = capture.read()
+
+    cv.imshow('video', frame)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
