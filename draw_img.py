c4f5c88939d30e9999fec6c9e1c03b0cdb38fc24
wessware <stevenshisia@yahoo.com>
2022-05-13 13:16:27 +0300

    line draw

--- draw_img.py
+++ draw_img.py
@@ -19,4 +19,11 @@ cv.rectangle(blank, (0, 0),
              (blank.shape[1]//2, blank.shape[0]//2), (255, 255, 0), thickness=cv.FILLED)
 cv.imshow('Rectangle', blank)
 
+# drawing a circle
+cv.circle(blank, (250, 250), 40, (0, 0, 255), thickness=-1)
+cv.imshow('Circle', blank)
+
+# draw line
+cv.line(blank, (100, 250), (300, 400), (0, 255, 0), thickness=3)
+cv.imshow('Line', blank)
 cv.waitKey(0)
