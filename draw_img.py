41f7de3e49ea4fd19b3448086e190f7108ea881c
wessware <stevenshisia@yahoo.com>
2022-05-13 12:53:01 +0300

    drawing green blank

--- draw_img.py
+++ draw_img.py
@@ -4,12 +4,14 @@ import cv2 as cv
 import numpy as np
 
 
-blank = np.zeros((500, 500), dtype='uint8')
+blank = np.zeros((500, 500, 3), dtype='uint8')
 
 cv.imshow('Blank', blank)
 
-img = cv.imread('./images/img_2.jpg')
-cv.imshow('Img', img)
+#img = cv.imread('./images/img_2.jpg')
+#cv.imshow('Img', img)
 
+blank[:] = 0, 255, 0
+cv.imshow('Green', blank)
 
 cv.waitKey(0)
