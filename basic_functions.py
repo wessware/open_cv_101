8f214101bf3ab3d99cee525fd04bed5b98f6e2ed
wessware <stevenshisia@yahoo.com>
2022-05-13 15:04:22 +0300

    edge cascade

--- basic_functions.py
+++ basic_functions.py
@@ -11,13 +11,14 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray Image 3', gray)
 """
 # image blur
+"""
 blur = cv.GaussianBlur(img, (11, 11), cv.BORDER_DEFAULT)
 cv.imshow('Blur', blur)
-
 """
+
 # Edge cascade
 cany = cv.Canny(img, 125, 175)
 cv.imshow('Canny', cany)
-"""
+# """
 
 cv.waitKey(0)
