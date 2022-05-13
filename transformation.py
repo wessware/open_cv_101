9392f2dc73c2c54f23c09c1e618f32e4a5acfa5c
wessware <stevenshisia@yahoo.com>
2022-05-13 15:39:11 +0300

    image rotation

--- transformation.py
+++ transformation.py
@@ -25,5 +25,24 @@ def translate(img, x, y):
 translated = translate(img, -100, 100)
 cv.imshow('Translated Image', translated)
 
+# image rotation
+
+
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2, height//2)
+
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+
+# positive angles for anticlockwise, negative angles for clockwise
+rotated = rotate(img, 45)
+cv.imshow('Rotated Image', rotated)
+
 
 cv.waitKey(0)
