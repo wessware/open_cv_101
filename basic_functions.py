bc6480bc08cd82c68ebc2cc8c129a607e38e6854
wessware <stevenshisia@yahoo.com>
2022-05-13 14:52:19 +0300

    basic function - blur

--- basic_functions.py
+++ basic_functions.py
@@ -4,7 +4,15 @@ import cv2 as cv
 img = cv.imread('./images/img_2.jpg')
 cv.imshow('Image 3', img)
 
+
+"""
 # converting image to gratscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray Image 3', gray)
+"""
+# image blur
+blur = cv.GaussianBlur(img, (11, 11), cv.BORDER_DEFAULT)
+cv.imshow('Blur', img)
+
+
 cv.waitKey(0)
