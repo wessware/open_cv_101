27bdd8f06bad742c85d98b5e60b7046129a01bba
wessware <stevenshisia@yahoo.com>
2022-05-13 13:07:09 +0300

    draw rectangle

--- draw_img.py
+++ draw_img.py
@@ -11,7 +11,12 @@ cv.imshow('Blank', blank)
 #img = cv.imread('./images/img_2.jpg')
 #cv.imshow('Img', img)
 
-blank[200:300, 300:400] = 255, 255, 0
-cv.imshow('Green', blank)
+#blank[200:300, 300:400] = 255, 255, 0
+#cv.imshow('Green', blank)
+
+# drawing a rectangle
+cv.rectangle(blank, (0, 0),
+             (blank.shape[1]//2, blank.shape[0]//2), (255, 255, 0), thickness=cv.FILLED)
+cv.imshow('Rectangle', blank)
 
 cv.waitKey(0)
