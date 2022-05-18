0f73c8320ac4780abd40c298c7df0221195f3ac0
wessware <stevenshisia@yahoo.com>
2022-05-18 08:46:40 +0300

    color spaces; BGR RGB comparison

--- spaces.py
+++ spaces.py
@@ -1,18 +1,28 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 
 img = cv.imread('./images/img_1.jpg')
 cv.imshow('Image I', img)
 
+
+plt.imshow(img)
+plt.show()
+
 """
 # BGR tp Grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Grayscale', gray)
-"""
+
 # BGR to HSV (Hue Saturation Value)
 
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 cv.imshow('HSV', hsv)
 
 
-cv.waitKey(0)
+# BGR to LAB (L*A*B)
+
+lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+cv.imshow('LAB', lab)
+"""
+# cv.waitKey(0)
