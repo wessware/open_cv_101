cb4e9d08f860f1c42e6f0df8c2e4110520da9cc0
wessware <stevenshisia@yahoo.com>
2022-05-18 08:53:47 +0300

    color spaces; HSV - BGR

--- spaces.py
+++ spaces.py
@@ -14,23 +14,27 @@ plt.show()
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Grayscale', gray)
 
+# BGR to LAB (L*A*B)
+
+lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+cv.imshow('LAB', lab)
+
+# BGR to RGB
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow('RGB', rgb)
+"""
+
 # BGR to HSV (Hue Saturation Value)
 
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 cv.imshow('HSV', hsv)
 
 
-# BGR to LAB (L*A*B)
+# HSV - BGR
+hsv_bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+cv.imshow('HSV -- BGR', hsv_bgr)
 
-lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
-cv.imshow('LAB', lab)
-"""
-# BGR to RGB
-
-rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-cv.imshow('RGB', rgb)
-
-plt.imshow(rgb)
-plt.show()
+# plt.imshow(rgb)
+# plt.show()
 
 cv.waitKey(0)
