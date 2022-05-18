ceaeb24c348a17adaa90f5d33384fa6917d6f799
wessware <stevenshisia@yahoo.com>
2022-05-18 09:28:08 +0300

    smoothing; median blur

--- smoothing.py
+++ smoothing.py
@@ -6,12 +6,17 @@ cv.imshow('Image 7', img)
 
 
 # BLuRRING
+
 # i. Averaging
-average = cv.blur(img, (7, 7))
+average = cv.blur(img, (3, 3))
 cv.imshow('Average BLur', average)
 
 # Gaussian BLur
-gauss = cv.GaussianBlur(img, (7, 7), 0)
+gauss = cv.GaussianBlur(img, (3, 3), 0)
 cv.imshow('Gaussain Blur', gauss)
 
+
+# Median BLur
+median = cv.medianBlur(img, 3)
+cv.imshow('Median Blur', median)
 cv.waitKey(0)
